988daa4a1870d0856b4664e853c92f2cc9c30563
Organizer Samuel <organizersamuel.os@gmail.com>
2022-02-05 19:02:08 +0100

    Phone Bill Calculator Restructured

--- src/Week_0/Java_Fundamentals/Exercise_4/PhoneBillCalculator.java
+++ src/Week_0/Java_Fundamentals/Exercise_4/PhoneBillCalculator.java
@@ -1,7 +1,5 @@
 package Week_0.Java_Fundamentals.Exercise_4;
 
-import java.util.Scanner;
-
 /**
  * Calculate the final total of someone's cell phone bill.
  * Allow the operator to input the plan fee and the number of the overage minutes
@@ -9,49 +7,100 @@ import java.util.Scanner;
  * Create seperate methods to calculate the tax, overage fees, and final total.
  * print the itemized bill.
  */
+
 public class PhoneBillCalculator {
-    static Scanner scanner = new Scanner(System.in);
+
+    private int id;
+    private double baseCost;
+    private int allottedMinutes;
+    private int minutesUsed;
+
+    public PhoneBillCalculator(){
+        id = 0;
+        baseCost = 79.99;
+        allottedMinutes = 800;
+        minutesUsed = 800;
+    }
+
+    public PhoneBillCalculator(int id){
+        this.id = id;
+        baseCost = 79.99;
+        allottedMinutes = 800;
+        minutesUsed = 800;
+    }
+
+    public PhoneBillCalculator(int id, double baseCost, int allottedMinutes, int minutesUsed){
+        this.id = id;
+        this.baseCost = baseCost;
+        this.allottedMinutes = allottedMinutes;
+        this.minutesUsed = minutesUsed;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public double getBaseCost(){
+        return baseCost;
+    }
+
+    public void setBaseCost(double baseCost){
+        this.baseCost = baseCost;
+    }
+
+    public int getAllottedMinutes(){
+        return allottedMinutes;
+    }
+
+    public void setAllottedMinutes(int minutes){
+        allottedMinutes = minutes;
+    }
+
+    public int getMinutesUsed(){
+        return minutesUsed;
+    }
+
+    public void setMinutesUsed(int minutes){
+        minutesUsed = minutes;
+    }
+
+    public double calculateOverage(){
+        if(minutesUsed <= allottedMinutes){
+            return 0;
+        }
+
+        double overageRate = 0.25;
+        double overageMinutes = minutesUsed - allottedMinutes;
+        return overageMinutes * overageRate;
+    }
+
+    public double calculateTax(){
+        double taxRate = 0.15;
+        return taxRate * (baseCost + calculateOverage());
+    }
+
+    public double calculateTotal(){
+        return baseCost + calculateOverage() + calculateTax();
+    }
+
+    public void printItemizedBill(){
+        System.out.println("ID: " + id);
+        System.out.println("Base Rate: $" + baseCost);
+        System.out.println("Overage Fee: $"
+                + String.format("%.2f", calculateOverage()));
+        System.out.println("Tax: $"
+                + String.format("%.2f", calculateTax()));
+        System.out.println("Total: $"
+                + String.format("%.2f", calculateTotal()));
+    }
 
     public static void main(String[] args) {
-        double charge = 0.25;
-
-        // Get plan fee and average minutes from user
-        double planFee = getFee();
-        double ovgMins = getOvgMins();
-        scanner.close();
-
-
-        double ovgTotal = getPercentageTotal(ovgMins);
-        double taxTotal = getTax(planFee + ovgTotal);
-
-        double finalTotal = planFee + ovgTotal + taxTotal;
-        System.out.println("Phone Bill Statement");
-        System.out.println("Plan: " + String.format("%.2f", planFee));
-        System.out.println("Overage: " + String.format("%.2f", ovgTotal));
-        System.out.println("Tax: " + String.format("%.2f", taxTotal));
-        System.out.println("Total: " + String.format("%.2f", finalTotal));
-
-    }
-
-    public static double getFee() {
-        System.out.println("Provide plan fee: ");
-        double planFee = scanner.nextDouble();
-        return planFee;
-    }
-
-    public static double getOvgMins() {
-        System.out.println("Provide average minutes: ");
-        double avarageMins = scanner.nextDouble();
-        return avarageMins;
-    }
-
-    public static double getPercentageTotal(double ovgMins) {
-        double charge = 0.25;
-        return ovgMins * charge;
-    }
-
-    public static double getTax(double subtotal) {
-        double taxPercentage = 0.15;
-        return subtotal * taxPercentage;
+        PhoneBillCalculator bill = new PhoneBillCalculator(123456);
+        bill.setMinutesUsed(1000);
+        bill.printItemizedBill();
     }
 }
